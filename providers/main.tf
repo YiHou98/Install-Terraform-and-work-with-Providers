d193374c181a44b603aa66dea589791bf3060a80
YiHou98 <72457542+YiHou98@users.noreply.github.com>
2023-02-16 08:44:27 +0000

    Update main.tf

--- providers/main.tf
+++ providers/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
-  access_key = "AKIATW3OB4632UNWLCLY"
-  secret_key = "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"
+  access_key = "access_key"
+  secret_key = "secret key"
 }
 
 resource "aws_sns_topic" "topic-us-east" {
